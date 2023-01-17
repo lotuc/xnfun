9fcdf7bc1e2d730a482a4a0038e21d7578e31049
lotuc <lotu.c@outlook.com>
2023-01-17 11:44:49 +0800

    chore: update api implenetation

--- src/main/clj/lotuc/xnfun/api.clj
+++ src/main/clj/lotuc/xnfun/api.clj
@@ -1,40 +1,38 @@
 (ns lotuc.xnfun.api
-  (:require [lotuc.xnfun.rpc.node :as n]
-            [lotuc.xnfun.rpc.impl :as impl]))
+  (:require [lotuc.xnfun.rpc.nodev2 :as n]))
 
 (defn start-node
-  [{:as node :keys [node-id node-options]
-    {:keys [heartbeat-interval-ms
-            heartbeat-lost-ratio]} :node-options}]
-  (impl/start-node node))
+  [{:as node :keys [node-id node-options]}]
+  (doto (n/make-node node)
+    (n/start-node-link!)
+    (n/start-heartbeat!)
+    (n/serve-remote-call!)
+    (n/start-heartbeat-listener!)))
 
 (defn stop-node [node]
-  (impl/stop-node node))
+  (doto node
+    (n/stop-heartbeat-listener!)
+    (n/stop-serve-remote-call!)
+    (n/stop-heartbeat!)
+    (n/stop-node-link!)))
 
 (defn add-function [node fun-name fun & opt]
-  (n/add-function node fun-name fun opt))
+  (n/add-function! node fun-name fun opt))
 
 (defn call-function
-  "
-  Optional Arguments
-  - `options.out-c`
-  - `req-meta`
-    - `:node-id`: call to given node.
-    - `:with-stacktrace`
-    - `:timeout-ms`
-    - `:hb-interval-ms`
-    - `:hb-lost-ratio`
-
-  Returns {:keys [in-c out-c res-future]}
-  "
   [node fun-name fun-params
-   & {:as options
-      :keys [req-meta out-c]
-      {:keys [node-id]} :req-meta}]
-  (impl/call-function node fun-name fun-params options))
+   & {:as options :keys [req-meta out-c]}]
+  (n/submit-remote-call! node fun-name fun-params options))
 
 (defn call
   [node fun-name fun-params & {:as options}]
-  (let [r (impl/call-function node fun-name fun-params options)
-        f (:res-future r)]
-    (future (:res @f))))
+  (let [r (call-function node fun-name fun-params options)
+        p (:res-promise r)]
+    (future
+      (let [{:as r :keys [status data]} @p]
+        (cond
+          (= status :ok) data
+          (= status :err) (throw (ex-info "execution error" r))
+          (= status :xnfun/err) (throw (ex-info "local error" r))
+          (= status :xnfun/remote-err) (throw (ex-info "remote error" r))
+          :else (throw (ex-info "unkown response" r)))))))
